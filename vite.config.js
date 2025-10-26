6477fa407803fec8db90b34258624c394a1f4325
Kinson <95203053+kinsonchk@users.noreply.github.com>
2025-10-26 18:51:03 -0400

    Deploy app to GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/Dynamic-Weather-App', // deploy to GitHub Pages
+
 })
